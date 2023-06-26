f1d0f8cccfc896b43e40ed240e800b81c9dd6be6
charole <rjs985939@gmail.com>
2023-06-27 00:18:15 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['next', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+  rules: {
+    // Add your ESLint rules here
+  },
+};
